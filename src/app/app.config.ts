74475696ed95637f1ebff5786ed814640dbd519d
Wayne Gakuo <waynejotham@gmail.com>
2026-01-14 16:58:28 +0300

    feat(firebase): integrate Firebase configuration and initialize with Angular project
    - Added Firebase initialization with environment-specific configurations.
    - Introduced environment file replacements for development setup.
    - Installed @angular/fire and related Firebase dependencies.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {environment} from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbZt4UoMUBur1f70-Vk3QaLKpwvQRXImg",
+    authDomain: "wandersgenie.firebaseapp.com",
+    projectId: "wandersgenie",
+    storageBucket: "wandersgenie.firebasestorage.app",
+    messagingSenderId: "385772763974",
+    appId: "1:385772763974:web:336161f246109996df4347",
+    measurementId: "G-VRSC5ZBWWC"
+  }
+};
